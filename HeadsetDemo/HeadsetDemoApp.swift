0239c8ba3ad6dd7251e5d25f6db6009851006458
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2022-11-15 15:47:05 +0100

    Allow more flexible testing of audio session and engine

--- HeadsetDemo/HeadsetDemoApp.swift
+++ HeadsetDemo/HeadsetDemoApp.swift
@@ -7,27 +7,74 @@ import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        app.beginReceivingRemoteControlEvents()
+        UIApplication.shared.beginReceivingRemoteControlEvents()
         return true
     }
 }
+
+class Observer: NSObject {
+    let action: () -> Void
+
+    init(action: @escaping () -> Void) {
+        self.action = action
+    }
+
+    override func observeValue(
+            forKeyPath keyPath: String?,
+            of object: Any?,
+            change: [NSKeyValueChangeKey: Any]?,
+            context: UnsafeMutableRawPointer?
+    ) {
+        if keyPath == "outputVolume" {
+            action()
+        }
+    }
+}
+
 
 @main
 struct HeadsetDemoApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
+    private let nowPlayingInfoCenter = MPNowPlayingInfoCenter.default()
     private let cmdCenter = MPRemoteCommandCenter.shared()
     private let audioEngine = AVAudioEngine()
     private let audioPlayerNode = AVAudioPlayerNode()
-    
+    @State private var observer: Observer? = nil
+
     @State private var isSetupped = false
     @State private var isRecording = false
 
     @State private var isAudioEngineRunning = false
     @State private var isAudioPlayerNodePlaying = false
+    @State private var isOtherAudioPlaying = false
+    @State private var inputNodeInputFormat: AVAudioFormat? = nil
+    @State private var inputNodeOutputFormat: AVAudioFormat? = nil
+    @State private var attachedNodes: [AVAudioNode] = []
 
+    @State private var preferredInput: AVAudioSessionPortDescription? = nil
     @State private var availableInputs: [AVAudioSessionPortDescription] = []
     @State private var audioData: [AVAudioPCMBuffer] = []
+    @State private var category: AVAudioSession.Category? = nil
+
+    func setupObserver() {
+        let audioSession: AVAudioSession = AVAudioSession.sharedInstance()
+
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            isAudioEngineRunning = audioEngine.isRunning
+            isAudioPlayerNodePlaying = audioPlayerNode.isPlaying
+            availableInputs = audioSession.availableInputs ?? []
+            category = audioSession.category
+            isOtherAudioPlaying = audioSession.isOtherAudioPlaying
+            preferredInput = audioSession.preferredInput
+            inputNodeInputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
+            inputNodeOutputFormat = audioEngine.inputNode.outputFormat(forBus: 0)
+            attachedNodes = audioEngine.attachedNodes.map { element -> AVAudioNode in element }
+        }
+
+        // observer = Observer { toggleRecording() }
+        // AVAudioSession.sharedInstance().addObserver(observer!, forKeyPath: "outputVolume", options: .new, context: nil)
+    }
 
     func playSound() {
         do {
@@ -40,7 +87,7 @@ struct HeadsetDemoApp: App {
             print("Unable to play sound: \(error)")
         }
     }
-    
+
     func playRecording() {
         do {
             try audioData.forEach { buffer in
@@ -53,57 +100,45 @@ struct HeadsetDemoApp: App {
         }
     }
 
+    func toggleRecording() {
+        if (isRecording) {
+            stopRecording()
+        } else {
+            startRecording()
+        }
+    }
+
     func startRecording() {
         audioData = []
+        print("Starting recording")
         isRecording = true
     }
 
     func stopRecording() {
+        print("Stopping recording")
         isRecording = false
     }
 
-    func setup() {
-        let audioSession: AVAudioSession = AVAudioSession.sharedInstance()
-
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            isAudioEngineRunning = audioEngine.isRunning
-            isAudioPlayerNodePlaying = audioPlayerNode.isPlaying
-            availableInputs = audioSession.availableInputs ?? []
-        }
-
-        do {
-            try audioSession.setCategory(
-                .playAndRecord,
-                mode: .default,
-                options: [.allowBluetooth]
-            )
-            try audioSession.setActive(true)
-        } catch {
-            print("Error setting audio session category: \(error)")
-        }
-
+    func setupAudioEngine() {
         audioEngine.attach(audioPlayerNode)
         audioEngine.connect(audioPlayerNode, to: audioEngine.outputNode, format: nil)
+
+    }
+
+    func installTap() {
+        print("Installing tap")
         audioEngine.inputNode.installTap(onBus: 0, bufferSize: 8000, format: nil) { buffer, time in
             if (isRecording) {
                 audioData.append(buffer)
             }
         }
-        
-        /**
-         * We start the `audioEngine` immediately to be able to start recording from the background.
-         * Otherwise, if we would start the `audioEngine` on headset button press from the background, the app would crash.
-         * See: https://stackoverflow.com/a/61347295/8170620
-         */
-        do {
-            try audioEngine.start()
-        } catch {
-            print("Error starting audio engine: \(error)")
-        }
-
-        setupCommandCenter()
-        isSetupped = true
     }
+
+    func removeTap() {
+        print("Removing tap")
+        audioEngine.inputNode.removeTap(onBus: 0)
+    }
+
 
     func setupCommandCenter() {
         /**
@@ -125,47 +160,155 @@ struct HeadsetDemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            if (isSetupped) {
-                Button("Play sound") {
-                    playSound()
-                }.padding(20)
-                
+            Text("AudioSession").bold().padding().onAppear {
+                setupObserver()
+                setupCommandCenter()
+            }
+
+            VStack {
+                Text("Other playing audio: \(isOtherAudioPlaying ? "yes" : "no")")
+                Text("Category: \(category?.rawValue ?? "nil")")
+                HStack {
+                    Button("Set category") {
+                        do {
+                            try AVAudioSession.sharedInstance().setCategory(
+                                    .playAndRecord,
+                                    mode: .default,
+                                    options: [.allowBluetooth]
+                            )
+                        } catch {
+                            print("Error setting audio session category: \(error)")
+                        }
+                    }
+                    Button("Activate") {
+                        do {
+                            try AVAudioSession.sharedInstance().setActive(true)
+                        } catch {
+                            print("Error enabling audio session: \(error)")
+                        }
+                    }
+                    Button("Deactivate") {
+                        do {
+                            try AVAudioSession.sharedInstance().setActive(false)
+                        } catch {
+                            print("Error disabling audio session: \(error)")
+                        }
+                    }
+                }
+            }
+
+            HStack {
+                Text("Recording").bold()
+
+                Text(isRecording ? "yes" : "no")
+
                 if (!audioData.isEmpty && !isRecording) {
-                    Button("Play recording") {
+                    Button("Play") {
                         playRecording()
-                    }.padding(20)
+                    }
                 }
-                
+
                 if (!isRecording) {
-                    Button("Start recording") {
+                    Button("Start") {
                         startRecording()
-                    }.padding(20)
+                    }
                 } else {
-                    Button("Stop recording") {
+                    Button("Stop") {
                         stopRecording()
-                    }.padding(20)
+                    }
                 }
-                
-                Text("Audio engine running: \(isAudioEngineRunning ? "yes" : "no")")
-                Text("Audio player node playing: \(isAudioPlayerNodePlaying ? "yes" : "no")")
-                
-                Text("Available Inputs:").bold().padding()
-                
+            }
+
+            Text("AudioEngine").bold().padding()
+
+            VStack {
+                VStack {
+                    Text("Audio engine running: \(isAudioEngineRunning ? "yes" : "no")")
+                    Text("Audio player node playing: \(isAudioPlayerNodePlaying ? "yes" : "no")")
+                    Text("Input node input format")
+                    Text(inputNodeInputFormat?.debugDescription ?? "-")
+                    Text("Input node output format")
+                    Text(inputNodeOutputFormat?.debugDescription ?? "-")
+                }
+
+                VStack {
+                    ForEach(attachedNodes, id: \.self) { (node: AVAudioNode) in
+                        HStack {
+                            Text(node.debugDescription).bold()
+                        }
+                                .frame(maxWidth: .infinity)
+                                .padding(10)
+                                .border(Color.black, width: 1)
+                    }
+                }
+                        .padding([.horizontal], 20)
+
+                HStack {
+                    Text("Engine").bold()
+                    Button("Setup") {
+                        setupAudioEngine()
+                    }
+                    if (isAudioEngineRunning) {
+                        Button("Stop") {
+                            audioEngine.stop()
+                        }
+                        Button("Pause") {
+                            audioEngine.pause()
+                        }
+                    } else {
+                        Button("Start") {
+                            do {
+                                try audioEngine.start()
+                            } catch {
+                                print("Error starting audio engine: \(error)")
+                            }
+                        }
+                    }
+                    Button("Reset") {
+                        audioEngine.reset()
+                    }
+                }
+
+                HStack {
+                    Text("Tap").bold()
+                    Button("Install") {
+                        installTap()
+                    }
+                    Button("Remove") {
+                        removeTap()
+                    }
+                }
+                if (isAudioEngineRunning) {
+                    Button("Play sound") {
+                        playSound()
+                    }
+                }
+            }
+
+            VStack {
+                Text("Available Inputs:").bold()
                 VStack {
                     ForEach(availableInputs, id: \.self) { (input: AVAudioSessionPortDescription) in
-                        HStack {
-                            Text(input.portName).bold()
+                        HStack {
+                            Button(input.portName) {
+                                do {
+                                    try AVAudioSession.sharedInstance().setPreferredInput(input)
+                                } catch {
+                                    print("Error setting preferred input: \(error)")
+                                }
+                            }
                             Text(input.uid)
                         }
-                            .frame(maxWidth: .infinity)
-                            .padding(10)
-                            .border(Color.black, width: 1)
+                                .frame(maxWidth: .infinity)
+                                .padding(10)
+                                .border(Color.black, width: 1)
                     }
                 }.padding([.horizontal], 20)
-            } else {
-                Button("Setup") {
-                    setup()
-                }.padding(20)
+            }
+                .padding([.top], 20)
+            HStack {
+                Text("Preferred Input").bold()
+                Text(preferredInput?.portName ?? "-")
             }
         }
     }
